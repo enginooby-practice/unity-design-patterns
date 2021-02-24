bd55c541f054ac3c83155b06d4aa5cd3058ff618
Hieu Ngo <enginoobz@gmail.com>
2021-02-24 03:09:35 +0100

    Implement InputHandler using Command pattern

---
+++ Assets/Topics/Command Pattern/InputHandler.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class InputHandler : MonoBehaviour
+{
+    [SerializeField] List<GameObject> actors = new List<GameObject>();
+    [ValueDropdown("actors")] [SerializeField] GameObject currentActor;
+    MovementCommand jumpCommand, kickCommand, punchCommand, goForwardsCommand, goBackwardsCommand, turnLeftCommand, turnRightCommand;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpCommand.Excecute();
+        }
+        else if (Input.GetKeyDown(KeyCode.K))
+        {
+            kickCommand.Excecute();
+        }
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
+            punchCommand.Excecute();
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            goForwardsCommand.Excecute();
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            goBackwardsCommand.Excecute();
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            turnLeftCommand.Excecute();
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            turnRightCommand.Excecute();
+        }
+    }
+    private void OnValidate()
+    {
+        Camera.main.GetComponent<CameraFollow360>().player = currentActor.transform;
+        Animator animator = currentActor.GetComponent<Animator>();
+
+        jumpCommand = new MovementCommand(animator, "isJumping");
+        kickCommand = new MovementCommand(animator, "isKicking");
+        punchCommand = new MovementCommand(animator, "isPunching");
+        goForwardsCommand = new MovementCommand(animator, "isWalking");
+        goBackwardsCommand = new MovementCommand(animator, "isWalking");
+        turnLeftCommand = new MovementCommand(animator, "isWalking");
+        turnRightCommand = new MovementCommand(animator, "isWalking");
+    }
+}
---
+++ Assets/Topics/Command Pattern/MovementCommand.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementCommand : Command
+{
+    protected Animator animator;
+    protected string trigger;
+
+    public MovementCommand(Animator animator, string trigger)
+    {
+        this.animator = animator;
+        this.trigger = trigger;
+    }
+    public override void Excecute()
+    {
+        if (animator != null) animator.SetTrigger(trigger);
+    }
+    public override void ExecuteReversely()
+    {
+        Debug.Log(">>> Execute reversely");
+        if (animator != null) animator.SetTrigger(trigger);
+    }
+}
+
+public class JumpCommand : MovementCommand
+{
+    public JumpCommand(Animator animator, string trigger) : base(animator, trigger) { }
+    public override void Excecute()
+    {
+        Debug.Log(">>> Jump");
+        if (animator != null) animator.SetTrigger(trigger);
+    }
+}
