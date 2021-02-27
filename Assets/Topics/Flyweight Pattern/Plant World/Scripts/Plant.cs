e900a6a8e6fcf8d8b1df68942b3c5078fe04f155
Hieu Ngo <enginoobz@gmail.com>
2021-02-27 18:38:55 +0100

    Enable outline for tree on mouse hover

--- Assets/Topics/Flyweight Pattern/Plant World/Scripts/Plant.cs
+++ Assets/Topics/Flyweight Pattern/Plant World/Scripts/Plant.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EPOOutline;
 
 public class Plant : MonoBehaviour
 {
     [SerializeField] PlantData info;
+    Outlinable outliner;
     PlantInfoPanel infoPanel;
+
     // Start is called before the first frame update
     void Start()
     {
         infoPanel = FindObjectOfType<PlantInfoPanel>();
+        outliner = GetComponent<Outlinable>();
     }
 
     // Update is called once per frame
@@ -17,7 +21,15 @@ public class Plant : MonoBehaviour
     {
 
     }
+    private void OnMouseEnter()
+    {
+        if (outliner) outliner.enabled = true;
+    }
 
+    private void OnMouseExit()
+    {
+        if (outliner) outliner.enabled = false;
+    }
     private void OnMouseDown()
     {
         infoPanel.SetPlantData(info);
