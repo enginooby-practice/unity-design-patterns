dc33c4f6d8f3123f22e7e95a6e30cd8de22a3ca8
Hieu Ngo <enginoobz@gmail.com>
2021-02-27 20:08:19 +0100

    Import warhog car model

--- Assets/Topics/Flyweight Pattern/Cubic World/CubicWorldGenerator.cs
+++ Assets/Topics/Flyweight Pattern/Cubic World/CubicWorldGenerator.cs
@@ -15,7 +15,7 @@ public class CubicWorldGenerator : MonoBehaviour
         {
             for (int z = 0; z < depth; z++)
             {
-                Vector3 pos = new Vector3(x, Mathf.PerlinNoise(x * 0.2f, z * 0.2f) * 3, z);
+                Vector3 pos = new Vector3(x, Mathf.PerlinNoise(x * 0.21f, z * 0.21f), z);
 
                 if (usePrefab)
                 {
