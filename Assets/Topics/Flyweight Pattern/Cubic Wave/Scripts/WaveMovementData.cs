cf5c6af9c275789158a6a35fe269e9e964c59fc8
Hieu Ngo <enginoobz@gmail.com>
2021-03-02 11:03:08 +0100

    [ECS] Cubic wave

---
+++ Assets/Topics/Flyweight Pattern/Cubic Wave/Scripts/WaveMovementData.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+
+[GenerateAuthoringComponent]
+public struct WaveMovementData : IComponentData
+{
+    public float amplitude;
+    public float xOffset;
+    public float yOffset;
+}
---
+++ Assets/Topics/Flyweight Pattern/Cubic Wave/Scripts/WaveSystem.cs
@@ -0,0 +1,15 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+public class WaveSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref Translation trans, ref WaveMovementData waveMovementData) =>
+        {
+            float zPos = math.sin((float)Time.ElapsedTime *5+ trans.Value.x * waveMovementData.xOffset + trans.Value.y * waveMovementData.yOffset);
+            trans.Value = new float3(trans.Value.x, trans.Value.y, zPos);
+        });
+    }
+}
